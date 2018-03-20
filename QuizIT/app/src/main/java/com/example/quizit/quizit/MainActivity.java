67e99381e46b029182d1317cd0fa0a96ecdbf72f
Lucas Dilan <ldstrigueiro@gmail.com>
2018-03-20 00:11:17 -0300

    App_v1.0_btnCadastrar(test)
    Acrescentei a troca de tela para a tela de cadastro, ao clicar no textView txtCadastrar na tela inicial.

--- QuizIT/app/src/main/java/com/example/quizit/quizit/MainActivity.java
+++ QuizIT/app/src/main/java/com/example/quizit/quizit/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.quizit.quizit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    private TextView txtCadastrar;
+    private Intent intentHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCadastrar = (TextView) findViewById(R.id.txtCadastrar);
+        txtCadastrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.txtCadastrar:
+                intentHome = new Intent(this, Home.class);
+                startActivity(intentHome);
+                break;
+
+        }
     }
 }
